4c7de76e65bbc4fb3065389775373ea09d2ac0bb
devanomaly <rafaelfpcosta@gmail.com>
2022-04-25 19:06:13 -0300

    less go

--- RatingsCounter.py
+++ RatingsCounter.py
@@ -1,4 +1,4 @@
-# Conta as classificações do dataset ml-100k (quantas sao 1 estrela, 2 estrelas etc...)
+# Count the classifications in dataset ml-100k (how many 1 star, 2 stars etc...)
 from pyspark import SparkContext
 import matplotlib.pyplot as plt
 import re
@@ -7,18 +7,16 @@ sc = SparkContext("local", "Ratings Counter")
 
 lines = sc.textFile("data/ml-100k/u.data")
 
-ratings = lines.map(lambda x: re.split("\t", x)[2])
+ratings = lines.map(lambda x: re.split("\t", x)[2]) #regex to split by tab
 
 results = ratings.countByValue()
 
-# ordenando o resultado com base no número de avaliações
 sortedResults = [(k, v) for k, v in sorted(results.items(), key=lambda item: item[1])]
 
-histogram = []
+# Now for some visualization
 for result in sortedResults:
     print(result)
     plt.scatter(result[0], result[1])
 plt.xlabel("Estrelas", fontsize=17)
 plt.ylabel("# avaliações", fontsize=17)
 plt.show()    
-# vemos que a maioria dos filmes foi avaliado em 4 estrelas
