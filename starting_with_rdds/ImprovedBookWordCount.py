dc392cc406471a9c792e09788253f81074baec7b
devanomaly <rafaelfpcosta@gmail.com>
2022-06-22 14:56:04 -0300

    CHECK -> intro to RDDs!

--- starting_with_rdds/ImprovedBookWordCount.py
+++ starting_with_rdds/ImprovedBookWordCount.py
@@ -10,7 +10,6 @@ words = book.flatMap(lambda x: re.split("\\W+", x)).map(lambda x: x.lower())
 
 wordsOccurences_sorted = words.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y).map(lambda x: (x[1], x[0])).sortByKey(ascending=False) #x[0] is word, x[1] is count
 
-
 # Lets sort the big-data way!
 for result in wordsOccurences_sorted.collect():
     print(result[1]," -- ", result[0])
---
+++ starting_with_rdds/TotalAmmountSpent.py
@@ -0,0 +1,26 @@
+# We'll compute the total ammount spent per costumer in a large dataset
+from pyspark import SparkContext
+import matplotlib.pyplot as plt
+
+sc = SparkContext("local", "Mean friends by Age")
+
+lines = sc.textFile("data/customer-orders.csv")
+
+
+def getIdAndAmmount(x):
+    splitted = x.split(",")
+    # first one is user_id, last is ammount in that particular order that's why we want'em
+    return splitted[0], float(splitted[-1])
+
+
+userAmmountPairs = lines.map(getIdAndAmmount)
+
+print("Number of orders =>", userAmmountPairs.count())
+totalsByUser = userAmmountPairs.reduceByKey(
+    lambda x, y: x + y
+).map(lambda x: (x[1], x[0])).sortByKey(ascending=False)
+
+print("Number of users =>", totalsByUser.count())
+print("UserID -- Total ammount spent")
+for result in totalsByUser.collect():
+    print(result[1]," -- ", round(result[0],2))
